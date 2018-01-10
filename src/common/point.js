ff02bff0f6d80c0c57dc5ccb4a833a1ecb667b2f
shitake <z1522716486@hotmail.com>
2018-01-10 20:13:52 +0800

    fix(common): fix point style && update point doc

--- src/common/point.js
+++ src/common/point.js
@@ -8,22 +8,22 @@ class Point {
      * @param {number} [y=0] - position of the point on the y axis
      */
     constructor (x = 0, y = 0) {
-        /**
-         * @member {number}
-         * @default 0
-         */        
-        this.x = x
-        /**
-         * @member {number}
-         * @default 0
-         */        
-        this.y = y
-    }
+      /**
+       * @member {number}
+       * @default 0
+       */        
+      this.x = x
+      /**
+       * @member {number}
+       * @default 0
+       */        
+      this.y = y
+  }
   
     /**
      * Returns the distance between two points.
      * 
-     * @param {Point} point
+     * @param {Point} point - Anther point object
      * @return {Number}
      */
     distance (point) {
@@ -34,7 +34,7 @@ class Point {
      * Returns the analytic distance([xDistance, yDistance]) between two points.
      * `Math.pow(point.distanceAxis[0], 2) + Math.pow(point.distanceAxis[1], 2) == Math.pow(point.distance, 2)`
      * 
-     * @param {Point} point
+     * @param {Point} point - Anther point object
      * @return {Array<Number, Number>}
      */
     distanceAxis (point) {
