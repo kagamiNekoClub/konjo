7b6165e2effcb8dd4aea00361e7cc3b5c921e80f
shitake <z1522716486@hotmail.com>
2018-01-11 18:17:46 +0800

    feat(boxes): add sphere and obb object

--- src/boxes/obb.js
+++ src/boxes/obb.js
@@ -1,5 +1,188 @@
+import Range from '../common/range'
+import Point from '../common/point'
+import Axes from '../common/axes'
 import Base from './base'
+import AABB from './aabb'
+import Sphere from './sphere'
 
-class OBB extends Base {}
+/**
+ * Oriented bounding box class.
+ *
+ */
+class OBB extends Base {
+  get x () { return this._x }
+  set x (value) {
+    this._x = value
+    this._create()
+  }
+
+  get y () { return this._y }
+  set y (value) {
+    this._y = value
+    this._create()
+  }
+
+  get width () { return this._width }
+  set width (value) {
+    this._width = value
+    this._create()
+  }
+
+  get height () { return this._height }
+  set height (value) {
+    this._height = value
+    this._create()
+  }
+
+  get angle () { return this._angle }
+  set angle (value) {
+    this._angle = value
+    this._create()
+  }
+
+  /**
+   * The axes of this obb.
+   *
+   * @return {*}
+   */
+  get axes () { return this._axes }
+
+  /**
+   * The overall width range of this obb.
+   * @return {Range}
+   */
+  get widthRange () { return this._widthRange }
+
+  /**
+   * The overall height range of this obb.
+   * @return {Range}
+   */
+  get heightRange () { return this._heightRange }
+
+  /**
+   * The center point of this obb.
+   * @return {Point}
+   */
+  get center () { return this._center }
+
+  /**
+   * Returns an oriented bounding box object.
+   * @param {Number} [x=0] - The X coordinate of the upper-left corner of the obb.
+   * @param {Number} [y=0] - The Y coordinate of the upper-left corner of the obb.
+   * @param {Number} [width=0] - The width of this obb.
+   * @param {Number} [height=0] - The height of this obb.
+   * @param {Number} [angle=0] - The overall height of this obb.
+   */
+  constructor (x = 0, y = 0, width = 0, height = 0, angle = 0) {
+    super()
+    this._x = x
+    this._y = y
+    this._width = width
+    this._height = height
+    this._angle = angle
+    this._create()
+  }
+
+  _create () {
+    this._center = new Point(this.x, this.y)
+    this._axes = new Axes(this.center, this.angle)
+    this._widthRange = new Range(-this.width / 2, this.width / 2)
+    this._heightRange = new Range(-this.height / 2, this.height / 2)
+  }
+
+  /**
+   * Get an array of point objects that span the four vertices of the obb.
+   *
+   * @return {Array<Point>}
+   */
+  points () {
+    let axes = this.axes.reAxes()
+    return [
+      axes.point(new Point(this.width / 2, this.height / 2)),
+      axes.point(new Point(-this.width / 2, this.height / 2)),
+      axes.point(new Point(this.width / 2, -this.height / 2)),
+      axes.point(new Point(-this.width / 2, -this.height / 2))
+    ]
+  }
+
+  /**
+   * Checks whether the point object given are contained within this obb.
+   *
+   * @param {Point} point - The point object of test.
+   */
+  pointCollision (point) {
+    point = this.axes.point(point)
+    return this.widthRange.hit(point.x) && this.heightRange.hit(point.y)
+  }
+
+  /**
+   * Checks whether the aabb object given are contained within this obb.
+   *
+   * @param {AABB} aabb - The aabb object of test
+   */
+  aabbCollision (aabb) {
+    if (this.angle % 90 === 0) {
+      return this.obb2aabb().aabbCollision(aabb)
+    } else {
+      return this.obbCollision(aabb.aabb2obb())
+    }
+  }
+
+  /**
+   * Checks whether the obb object given are contained within this obb.
+   *
+   * @param {OBB} obb - The obb object of test
+   */
+  obbCollision (obb) {
+    let obbInCenter = this.axes.point(obb.center)
+    let inObbCenter = obb.axes.point(this.center)
+    let owRange = new Range(obbInCenter.x - obb.width / 2, obbInCenter.x + obb.width / 2)
+    let ohRange = new Range(obbInCenter.y - obb.height / 2, obbInCenter.y + obb.height / 2)
+    let woRange = new Range(inObbCenter.x - this.width / 2, inObbCenter.x + this.width / 2)
+    let hoRange = new Range(obbInCenter.y - this.height / 2, obbInCenter.y + this.height / 2)
+    let wRangeCollision = this.widthRange.collision(owRange)
+    let hRangeCollision = this.heightRange.collision(ohRange)
+    let obbWidthRangeCollision = obb.widthRange.collision(woRange)
+    let obbHeightRangeCollision = obb.heightRange.collision(hoRange)
+    return wRangeCollision && hRangeCollision && obbWidthRangeCollision && obbHeightRangeCollision
+  }
+
+  /**
+   * Checks whether the sphere object given are contained within this obb.
+   *
+   * @param {Sphere} sphere - The sphere object of test
+   */
+  sphereCollision (sphere) {
+    return sphere.obbCollision(this)
+  }
+
+  /**
+   * Transforms this object into a new AABB object and returns.
+   *
+   * @return {AABB}
+   */
+  obb2aabb () {
+    let points = this.points()
+    let maxX, maxY, minX, minY
+    maxX = maxY = minX = minY = 0
+    points.forEach((point) => {
+      if (point.x > maxX) maxX = point.x
+      if (point.x > minX) minX = point.x
+      if (point.y > maxY) maxY = point.y
+      if (point.y > minY) minY = point.y
+    })
+    return new AABB(minX, minY, maxX - minX, maxY - minY)
+  }
+
+  /**
+   * Transforms this object into a new Sphere object and returns.
+   *
+   * @return {Sphere}
+   */
+  obb2sphere () {
+    let radius = Math.ceil(Math.sqrt((this.width / 2) ** 2 + (this.height / 2) ** 2))
+    return new Sphere(this.x, this.y, radius)
+  }
+}
 
 export default OBB
--- src/boxes/sphere.js
+++ src/boxes/sphere.js
@@ -1,5 +1,125 @@
+import Range from '../common/range'
+import Point from '../common/point'
+import Vector2 from '../common/vector2'
 import Base from './base'
+import AABB from './aabb'
+import OBB from './obb'
 
-class Sphere extends Base {}
+/**
+ * Sphere box class.
+ *
+ */
+class Sphere extends Base {
+  get x () { return this._x }
+  set x (value) {
+    this._x = value
+    this._create()
+  }
+
+  get y () { return this._y }
+  set y (value) {
+    this._y = value
+    this._create()
+  }
+
+  get radius () { return this._radius }
+  set radius (value) {
+    this._radius = value
+    this._create()
+  }
+
+  /**
+   * The overall width range of this obb.
+   * @return {Range}
+   */
+  get widthRange () { return this._widthRange }
+
+  /**
+   * The overall height range of this obb.
+   * @return {Range}
+   */
+  get heightRange () { return this._heightRange }
+
+  /**
+   * Returns an sphere box object.
+   * @param {Number} [x=0] - The X coordinate of the upper-left corner of the sphere.
+   * @param {Number} [y=0] - The Y coordinate of the upper-left corner of the sphere.
+   * @param {Number} [radius=0] - The width of this obb.
+   */
+  constructor (x = 0, y = 0, radius = 0) {
+    super()
+    this._x = x
+    this._y = y
+    this._radius = radius
+    this._create()
+  }
+
+  _create () {
+    this._widthRange = new Range(this.x - this.radius, this.x + this.radius)
+    this._heightRange = new Range(this.y - this.radius, this.y + this.radius)
+  }
+
+  /**
+   * Checks whether the point object given are contained within this sphere.
+   *
+   * @param {Point} point - The point object of test.
+   */
+  pointCollision (point) {
+    return (point.x - this.x) ** 2 + (point.y - this.y) ** 2 <= this.radius ** 2
+  }
+
+  /**
+   * Checks whether the aabb object given are contained within this sphere.
+   *
+   * @param {AABB} aabb - The aabb object of test
+   */
+  aabbCollision (aabb) {
+    let sphereCenter2aabbCenter = (new Vector2(this.x, this.y)).sub(new Vector2(aabb.center.x, aabb.center.y)).abs()
+    let aabbPoint = aabb.points[1]
+    let aabbHalfLength = (new Vector2(aabbPoint.x, aabbPoint.y)).sub(new Vector2(aabb.center.x, aabb.center.y)).abs()
+    let sphereCenter2aabb = sphereCenter2aabbCenter.sub(aabbHalfLength).max(0)
+    return sphereCenter2aabb.dot() <= this.radius ** 2
+  }
+
+  /**
+   * Checks whether the obb object given are contained within this sphere.
+   *
+   * @param {OBB} obb - The obb object of test
+   */
+  obbCollision (obb) {
+    let point = obb.axes.point(new Point(this.x, this.y))
+    let sphereCenter2obbCenter = (new Vector2(point.x, point.y)).sub(new Vector2(0, 0)).abs()
+    let obbHalfLength = new Vector2(obb.width / 2, obb.height / 2)
+    let sphereCenter2obb = sphereCenter2obbCenter.sub(obbHalfLength).max(0)
+    return sphereCenter2obb.dot() <= this.radius ** 2
+  }
+
+  /**
+   * Checks whether the sphere object given are contained within this sphere.
+   *
+   * @param {Sphere} sphere - The sphere object of test
+   */
+  sphereCollision (sphere) {
+    return this.pointCollision(new Point(sphere.x, sphere.y))
+  }
+
+  /**
+   * Transforms this object into a new AABB object and returns.
+   *
+   * @return {AABB}
+   */
+  sphere2aabb () {
+    return new AABB(this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2)
+  }
+
+  /**
+   * Transforms this object into a new OBB object and returns.
+   *
+   * @return {OBB}
+   */
+  sphere2obb (angle = 0) {
+    return new OBB(this.x, this.y, this.radius, this.radius, angle)
+  }
+}
 
 export default Sphere
